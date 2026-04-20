7cd32828d065fad5e5ac1fc2a7a9a2e9cebdf831
josue <josue@DESKTOP-TAOQC4P>
2026-04-19 19:03:40 -0600

    Add .gitignore and remove build/system files

--- EI/EI.Web/obj/Debug/net9.0/EI.Web.AssemblyInfo.cs
+++ EI/EI.Web/obj/Debug/net9.0/EI.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EI.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d099cf63e5b883ebdb1fc7ca6e1325fe8b5394")]
 [assembly: System.Reflection.AssemblyProductAttribute("EI.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EI.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
